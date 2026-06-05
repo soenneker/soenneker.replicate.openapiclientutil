9213299ac3c6adce132b0e6bce020c622e7ce174
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:41 -0500

    xml commenting

--- src/Soenneker.Replicate.OpenApiClientUtil/Abstract/IReplicateOpenApiClientUtil.cs
+++ src/Soenneker.Replicate.OpenApiClientUtil/Abstract/IReplicateOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Replicate.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface IReplicateOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<ReplicateOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
